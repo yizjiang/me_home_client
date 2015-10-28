3f79100b024a03e32be933e3c2cce9471ec85433
yizjiang <leo.jyz@gmail.com>
2015-10-27 21:19:40 -0700

    saf

--- assets/js/components/agent/header.js
+++ assets/js/components/agent/header.js
@@ -12,17 +12,17 @@ var Header = React.createClass({
 
   render: function () {
 
-    var value = {};
-    if(this.props.header){
-      value = this.props.header;
-    }
+//    var value = {};
+//    if(this.props.header){
+//      value = this.props.header;
+//    }
 
     return (
       <div>
         <label className='agentlabel'>姓名</label>
-        <input id='name' type="text" value={value.name} placeholder="姓名"/>
+        <input id='name' type="text" placeholder="姓名"/>
         <label className='agentlabel'>联系方式</label>
-        <input id='contact' type="text" value={value.contact} placeholder="联系方式"/>
+        <input id='contact' type="text" placeholder="联系方式"/>
         <Button bsStyle='success' className='save_header' onClick={this.saveHeader.bind(this, '123')}>保存联系方式</Button>
       </div>
       );
