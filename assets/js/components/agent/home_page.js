58ebe06e1329eae0023d50338b153ce917d8921a
yizjiang <leo.jyz@gmail.com>
2016-01-21 14:38:46 -0800

    style

--- assets/js/components/agent/home_page.js
+++ assets/js/components/agent/home_page.js
@@ -35,7 +35,7 @@ var HomePage = React.createClass({
     return (
       <div>
         <div className='agent_home_div'>
-          <img src={this.props.data.head_image}></img>
+          <img className='profile_img' src={this.props.data.head_image}></img>
           <p>姓名: {this.props.data.header.name}</p>
           <p>联系电话: {this.props.data.header.contact}</p>
           <img src={this.props.data.qr_image}></img>
--- assets/js/components/home/public_record.js
+++ assets/js/components/home/public_record.js
@@ -3,29 +3,30 @@ var React = require('react');
 var PublicRecord = React.createClass({
 
   render: function() {
-    console.log(this.props);
     var {record, diff} = this.props;
     return (
-      <div>
-        <h3>最近成交记录</h3>
-        <div>
-          <div>
-           <p>{record.property_id}</p>
-          </div>
-          <div>
-            <p>{record.price}</p>
-          </div>
-          <div>
-            <p>{record.record_date}</p>
-          </div>
-          <div>
-            <p>{record.source}</p>
-          </div>
-          <div>
-            <p>每年涨幅</p>
-            <p>{diff}%</p>
-          </div>
-       </div>
+      <div className='public-record-wrap'>
+        <h3>最近挂牌记录</h3>
+        <div className='public-record-detail'>
+         <h4>交易信息</h4>
+         <p>{record.source}: {record.property_id}</p>
+        </div>
+        <div className='public-record-detail'>
+          <h4>交易状态</h4>
+          <p>{record.event}</p>
+        </div>
+        <div className='public-record-detail' >
+          <h4>日期</h4>
+          <p>{record.record_date}</p>
+        </div>
+        <div className='public-record-detail'>
+          <h4>价格</h4>
+          <p>{record.price}</p>
+        </div>
+        <div className='public-record-detail'>
+          <h4>每年涨幅</h4>
+          <p>{diff}%</p>
+        </div>
       </div>
     );
   }
