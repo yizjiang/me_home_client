55ddc7039027504f3885a40a173ad48057262ae7
yizjiang <leo.jyz@gmail.com>
2016-12-05 20:53:33 -0800

    region file

---
+++ dist/wechat/region_tutorial.js
@@ -0,0 +1,82 @@
+var data =
+[
+  {
+    area: '湾区',
+    regions: [
+      {
+        title: '华人最多',
+        cities: [
+          {
+            name: '福斯特城',
+            price: '7万人民币/平方米',
+            summary: '半岛华人聚集区',
+            en: 'Foster City'
+          },
+          {
+            name: '费里蒙德',
+            price: '3万人民币/平方米',
+            summary: '东湾华人聚集区',
+            en: 'Fremont'
+          },
+          {
+            name: '山景城',
+            price: '6万人民币/平方米',
+            summary: '谷歌,领英等大公司',
+            en: 'Mountain View'
+          }
+        ]
+      },
+
+      {
+        title: '科技园区',
+        cities: [
+          {
+            name: '圣弗朗西斯科',
+            price: '17万人民币/平方米',
+            summary: '创业公司',
+            en: 'San Francisco'
+          },
+          {
+            name: '费里蒙德',
+            price: '3万人民币/平方米',
+            summary: '东湾华人聚集区',
+            en: 'Fremont'
+          },
+
+          {
+            name: '山景城',
+            price: '6万人民币/平方米',
+            summary: '谷歌,领英等大公司',
+            en: 'Mountain View'
+          }
+        ]
+      },
+
+      {
+        title: '有钱人的社区',
+        cities: [
+          {
+            name: '艾瑟顿',
+            price: '17万人民币/平方米',
+            summary: '都是有钱人',
+            en: 'Atherton'
+          },
+
+          {
+            name: '希尔斯堡',
+            price: '20万人民币/平方米',
+            summary: '全都是有钱人',
+            en: 'Hillsborough'
+          },
+
+          {
+            name: '帕里奥托',
+            price: '13万人民币/平方米',
+            summary: '科技大佬',
+            en: 'Palo Alto'
+          }
+        ]
+      }
+    ]
+  }
+]
